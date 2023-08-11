ad133d48e29127cb4d1c88083b4726ec9b0bbabc
Ziko909 <zikoziko078@gmail.com>
2023-08-12 00:07:54 +0100

    feat : add Blogs component to the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Navbar from "./Navbar"
+import Blogs from "./Blogs"
+
+export interface BlogList {
+  title : string,
+  body: string,
+  id: number
+}
 
 function App() : JSX.Element {
+      const blogs : BlogList[] = [{
+        title: "My first Blog", body: "written in january", id: 1
+    }, {
+        title: "My second Blog", body: "written in mars", id: 2
+    }, {
+        title: "My third Blog", body: "written in aout", id: 3
+    }
+    ]
   return (
     <div className="App">
       <Navbar />
+      <Blogs blogs={blogs}/>
     </div>
   );
 }
 
-export default App; 
+export default App;
