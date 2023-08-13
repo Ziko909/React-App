be49c3e203c9700b3130082813c048d404f58731
Ziko909 <zikoziko078@gmail.com>
2023-08-13 23:21:42 +0100

    feat : create a new component for individual blog item

---
+++ src/Blog.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import './Blog.css'
+
+
+export interface BlogType {
+    title : string,
+    body: string,
+    id: number
+  }
+
+interface BlogItem {
+    blog: BlogType;
+}
+
+function Blog(props: BlogItem) {
+    const blog : BlogType = props.blog;
+    return (
+        <div className="blog_item" key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p>{blog.body}</p>
+        </div>
+    )
+}
+
+export default Blog; 
