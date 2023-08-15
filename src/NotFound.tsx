89cd48a358d4c58a6362ed602e24bd9de0456ecf
Ziko909 <zikoziko078@gmail.com>
2023-08-15 03:17:08 +0100

    feat : create NotFound Component.

---
+++ src/NotFound.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./NotFound.css"
+
+
+function NotFound() {
+    return (
+        <div className="not_found">
+            <h1> Page Not Found </h1>
+        </div>
+    )
+}
+
+export default NotFound;
