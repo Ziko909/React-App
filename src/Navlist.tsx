1755bff15e2d1d7855eb14b86256ced7e73772b1
Ziko909 <zikoziko078@gmail.com>
2023-08-15 03:15:42 +0100

    feat : add links to navigation list

--- src/Navlist.tsx
+++ src/Navlist.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "./Navlist.css"
+import { Link } from "react-router-dom";
 
 
 
 function Navlist() : JSX.Element {
     const navList : string[] = ["Home", "New Blog"];
+    const map = new Map();
+
+    map.set(navList[0], '/');
+    map.set(navList[1], '/create');
+
     return (
         <ul className="navlist">
-            {navList.map(link => <li key={link}><a href="">{link}</a></li>)}
+            {navList.map(link => <li key={link}><Link to={map.get(link)}>{link}</Link></li>)}
         </ul>
     );
 }
