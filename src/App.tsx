a9baa2e05332925da1ec9df0da62c85ae1f45836
Ziko909 <zikoziko078@gmail.com>
2023-08-15 03:17:41 +0100

    feat : add Create and NotFound Components to the Route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Navbar from "./Navbar"
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import BlogDetails from './BlogDetails';
+import Create from './Create';
+import NotFound from './NotFound'
 
 
 
@@ -19,6 +21,12 @@ function App() : JSX.Element {
           </Route>
           <Route path="/blog/:id">
             <BlogDetails />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="*">
+            <NotFound />
           </Route>
         </Switch>
       </div>
