e0647d7aec482dcc208898d0f3df19ccd19dd0d6
Ziko909 <zikoziko078@gmail.com>
2023-08-15 03:12:48 +0100

    feat :  add delete option to the blog

--- src/BlogDetails.tsx
+++ src/BlogDetails.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import useFetch from "./useFetch";
 import Blog from "./Blog";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import "./BlogDetails.css"
 
 interface Params {
@@ -10,12 +10,23 @@ interface Params {
 function BlogDetails() {
     const { id } = useParams<Params>();
     const {data: blog, isLoading, error} = useFetch(`http://localhost:5000/blogs/${id}`);
+    const history = useHistory();
 
+    const onClickHundler = (e : any) => {
+        const request = {
+            method: 'DELETE',
+        }
+        const deleteBlog = fetch(`http://localhost:5000/blogs/${id}`, request);
+        deleteBlog.then(() => {
+            history.push('/');
+        })
+    }
     return (
         <div className="blog_details">
             { isLoading && <div className="loading">is Loading...</div> }
             { error && <div className="error"> {error} </div> }
-            { blog && <Blog blog={blog} />}
+            { blog && <Blog blog={blog} /> }
+            { blog && <button onClick={onClickHundler}>Delete</button> }
         </div>
     )
 }
