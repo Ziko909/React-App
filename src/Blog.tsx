4b2d1966c24747dfec94dc83b1d85f17867873ff
Ziko909 <zikoziko078@gmail.com>
2023-08-15 03:10:13 +0100

    feat : add auther and title and the body of the blog

--- src/Blog.tsx
+++ src/Blog.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import './Blog.css'
+import { Link } from "react-router-dom";
 
 
 export interface BlogType {
     title : string,
     body: string,
+    auther: string,
     id: number
   }
 
@@ -16,8 +18,10 @@ function Blog(props: BlogItem) {
     const blog : BlogType = props.blog;
     return (
         <div className="blog_item">
-            <h2>{blog.title}</h2>
-            <p>{blog.body}</p>
+            <h2>title : {blog.title}</h2>
+            <h4>Auther : {blog.auther}</h4>
+            <h4> Body : </h4>
+            <p> {blog.body} </p>
         </div>
     )
 }
